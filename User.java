134ad7673f6cce829e1c919a27a8523b8e0c4c38
Mahmoud <Mahmoud@gmail.com>
2021-12-07 19:40:55 +0200

    Database and User classes

---
+++ User.java
@@ -0,0 +1,11 @@
+package sprint1;
+import java.net.PasswordAuthentication;
+import java.util.Scanner;
+
+public abstract class User {
+    protected String name;
+    protected String MobileNum;
+    protected String Email;
+    protected String Password;
+    public boolean Suspended = false;
+}
